33b53c8635996eb48aed337d7975c6eceacff8f0
Shelly Tang <shellytang@gmail.com>
2017-05-15 15:40:32 -0700

    added middleware and models, updated package.json

---
+++ models/story.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const storySchema = Schema({
+  userId: [{type: Schema.Types.ObjectId, required: true, ref: 'user'}],
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  created: {type: Date, default: Date.now, required: true},
+  lastUpdated: {type: Date}, //NOTE stretch
+  genre: {type: String, required: true},
+  snippets: [{type: Schema.Types.ObjectId, ref: 'snippet'}],
+  snippetCount: {type: Number, default: 0},
+  pendingSnippets: [{type: Schema.Types.ObjectId, ref: 'snippet'}],
+  pendingSnippetCount: {type: Number, default: 0},
+});
+
+module.exports = mongoose.model('story', storySchema);

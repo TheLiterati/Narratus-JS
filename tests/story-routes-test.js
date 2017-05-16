b92aaee5d4ea18cdf34f4816eef9c4cd629dc586
Allie Grampa <agrampa@yahoo.com>
2017-05-15 18:08:37 -0700

    scaffolded story tests

--- tests/story-routes-test.js
+++ tests/story-routes-test.js
@@ -48,14 +48,171 @@ describe('Story routes', function() {
     .catch(() => done());
   });
   
+  describe('Unregistered route', function() {
+    it('should respond with a status of 404 not found', done => {
+      
+      
+      done();
+    });
+  });
   
-  describe('POST /api/story', function() {});
+  describe('POST /api/story', function() {
+    describe('Request with a valid body', function() {
+      it('should return a story', done => {
+        
+        done();
+      });
+    });
+    
+    describe('Request with an invalid token', function() {
+      it('should respond with a 401 unauthorized error', done => {
+        
+        done();
+      });
+    });
+    
+    describe('Request with an invalid body', function() {
+      it('should respond with a 400 invalid body error', done => {
+        
+        done();
+      });
+    });
+  });
   
-  describe('GET /api/story', function() {});
+  describe('GET /api/story', function() {
+    beforeEach(done => {
+      exampleStory.userId = this.tempUser._id.toString();
+      new Story(exampleStory).save()
+      .then(story => {
+        this.tempStory = story;
+        done();
+      })
+      .catch(() => done());
+    });
+    
+    afterEach(() => delete exampleStory.userId);
+    
+    describe('Request with a valid body', function() {
+      it('should return a story', done => {
+        
+        done();
+      });
+    });
+    
+    describe('Request with invalid token', function() {
+      it('should respond with a 401 unauthorized error', done => {
+        
+        
+        done();
+      });
+    });
+    
+    describe('Request with invalid id', function() {
+      it('should respond with a 404 not found error', done => {
+        
+        done();
+      });
+    });
+  });
   
-  describe('GET /api/story/:storyId', function() {});
+  describe('GET /api/story/:storyId', function() {
+    beforeEach(done => {
+      exampleStory.userId = this.tempUser._id.toString();
+      new Story(exampleStory).save()
+      .then(story => {
+        this.tempStory = story;
+        done();
+      })
+      .catch(() => done());
+    });
+    
+    afterEach(() => delete exampleStory.userId);
+    
+    describe('Request with a valid body', function() {
+      it('should return a story', done => {
+        
+        done();
+      });
+    });
+    
+    describe('Request with invalid token', function() {
+      it('should respond with a 401 unauthorized error', done => {
+        
+        
+        done();
+      });
+    });
+    
+    describe('Request with invalid id', function() {
+      it('should respond with a 404 not found error', done => {
+        
+        done();
+      });
+    });
+  });
   
-  describe('PUT /api/story/:storyId', function() {});
+  describe('PUT /api/story/:storyId', function() {
+    let updatedStory = {
+      title: 'Did we survive project week?',
+      description: 'A reflection on how the week went',
+      startSnippet: 'We are not sure yet, check back on Friday!',
+    };
+    
+    beforeEach(done => {
+      exampleStory.userId = this.tempUser._id.toString();
+      new Story(exampleStory).save()
+      .then(story => {
+        this.tempStory = story;
+        done();
+      });
+    });
+    
+    afterEach(() => delete exampleStory.userId);
+    
+    describe('A request with a valid body', function() {
+      it('should return an updated story', done => {
+        
+        done();
+      });
+    });
+    
+    describe('A request with an unauthorized token', function() {
+      it('should return an error of 401 unauthorized', done => {
+        
+        done();
+      });
+    });
+    
+    describe('A request with an invalid body', function() {
+      it('should return an error of 400 invalid body', done => {
+        
+        done();
+      });
+    });
+    
+    describe('A request with an invalid id', function() {
+      it('should return an error of 404 not found', done => {
+        
+        done();
+      });
+    });
+  });
   
-  describe('DELETE /api/story/:storyId', function() {});
+  describe('DELETE /api/story/:storyId', function() {
+    beforeEach(done => {
+      exampleStory.userId = this.tempUser._id.toString();
+      new Story(exampleStory).save()
+      .then(story => {
+        this.tempStory = story;
+        done();
+      });
+    });
+    
+    afterEach(() => delete exampleStory.userId);
+    
+    it('should remove the story and return a status of 204', done => {
+      
+      done();
+    });
+  });
 });

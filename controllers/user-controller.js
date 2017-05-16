a4b2f7665263d58691c04a161b1e7b1f36ef389e
Michael Padget <mpadget@comcast.net>
2017-05-15 17:08:47 -0700

    Working on user routes and controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const debug = require('morgan');
+const createError = require('http-errors');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const User = require('../models/user');
+mongoose.Promise = Promise;
+
+module.exports = exports = {};
+
+exports.createUser = function(user, req) {
+  debug('#createUser');
+  if(!user) return Promise.reject(createError(400, 'No user, user required.'));
+
+  let tempPassword = req.body.password;
+  req.body.password = null;
+  delete req.body.password;
+
+  let newUser = new User(user);
+  return newUser.generatePasswordHash(tempPassword)
+  .then(user => user.save())
+  .then(user => user.generateToken())
+  .then(token => Promise.resolve(token))
+  .catch(err => Promise.reject(err.status).send(err));
+};
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,34 +1,48 @@
 'use strict';
 
+const debug = require('debug')('narratus:auth-routes');
+const basicAuth = require('../lib/basic-auth-middleware');
+const User = require('../models/user');
+const userController = require('../controllers/user-controller');
+
 // Create user: email, username, password.
 //POST /api/signup
 
-// Will change router.use to post.
 module.exports = function(router) {
-  router.use('/signup', (req, res, next) => {
-    console.log('Signup middleware');
-    next();
+  router.post('/signup', (req, res) => {
+    debug('POST /signup');
+
+    return userController.createUser(req.body)
+    .then(token => {
+      console.log('token', token);
+      res.json(token);
+    })
+    .catch(err => res.status(err.status).send(err));
   });
+
+  // User login: username, password.
+  //GET /api/signin
+
+  router.get('/signin', basicAuth, (req, res) => {
+    debug('GET /signin');
+
+    return User.findOne({username: req.auth.username})
+    .then(user => user.comparePasswordHash(req.auth.password))
+    .then(user => user.generateToken())
+    .then(token => res.json(token))
+    .catch(err => res.status(err.status).send(err));
+  });
+
+  // router.get('/dashboard', (req, res) => {
+  //   debug('GET /dashboard');
+  //
+  //   return User.find({username: req.followedStories});
+  // });
   return router;
 };
 
-// app.use(function(req, res, next) {
-//   console.log('First middleware');
-//   next();
-// });
-
-// app.use('/signup', function(req, res, next) {
-//   console.log('Signup middleware');
-//   next();
-// });
-
-// User login: username, password.
-//GET /api/signin
-
-// app.use('/signin', function(req, res, next) {
-//   console.log('Signin middleware');
-//   next();
-// });
-
 //GET /api/dashboard
+// fetch all of the user's followed and owned stories.
+
 //PUT /api/follow/:storyId
+// follow a story that you are not contributing to.

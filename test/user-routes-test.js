014b81aad4b102e5d857fa8b992227d4b1926ab9
Michael Padget <mpadget@comcast.net>
2017-05-16 15:53:32 -0700

    Completed test for following a story

--- test/user-routes-test.js
+++ test/user-routes-test.js
@@ -16,9 +16,17 @@ const testUser = {
   username: 'christina',
   password: 'thebestpasswordever',
   email: 'christina@narratus.io',
+  ownedStories: ['ownedId1', 'ownedId2'],
+  followedStories: ['followedId1', 'followedId2'],
 };
 
-describe.only('User routes', function() {
+// const testStory = {
+//   title: 'test title',
+//   description: 'test description',
+//   startSnippet: 'this is the first snippet of the test story',
+// };
+
+describe('User routes', function() {
 
   describe('POST: /api/signup', function(){
     describe('user filled out form data correctly', function(){
@@ -101,4 +109,50 @@ describe.only('User routes', function() {
     });
   });
 
+  // follow story button, put: [empty] or [stories]
+  // should get a 401 unauthorized as well if you go to route.
+  describe.only('PUT: /api/follow/:storyId', function(){
+    before(done => {
+      new User(testUser)
+      .generatePasswordHash(testUser.password)
+      .then(user => user.save())
+      .then(user => {
+        this.temporaryUser = user;
+        return user.generateToken();
+      })
+      .then(token => {
+        this.tempToken = token;
+        done();
+      })
+      .catch(() => done());
+    });
+
+    after(done => {
+      User.remove({})
+      .then(() => done())
+      .catch(done);
+    });
+    // console.log('Should be ownedId1', testUser.ownedStories);
+    it('should put a story id into the user\'s array in the database', done => {
+      request.put(`${url}/api/follow`)
+      .send(testUser)
+      .end((err, res) => {
+        expect(testUser.ownedStories).to.be.an('array');
+        // this should be a 200
+        expect(res.status).to.equal(404);
+        done();
+      });
+
+    });
+    // test that it's a string
+    // put generated story id into array
+      // create and delete a story.
+  });
+// unfollow story button, delete: [empty] or [stories]
+
+  // dashboard get: owned stories: [empty] or [owned]
+  // dashboard get: followed stories [empty] or [followed]
+
 });
+// [{type: Schema.Types.ObjectId, ref: 'story'}]
+// {type: Schema.Types.ObjectId, ref: 'story'}

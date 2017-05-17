61e0cd1b0c5385bccee983e21287b700ac35889f
Shelly Tang <shellytang@gmail.com>
2017-05-17 09:09:50 -0700

    scaffolded test for snippets

--- test/snippet-routes-test.js
+++ test/snippet-routes-test.js
@@ -25,103 +25,105 @@ const exampleStory = {
 };
 
 const exampleSnippet = {
-  snippetContent: 'And then the storied continued with a user submitted snippet',
+  snippetContent: 'And then the story continued with a user submitted snippet',
 };
 
 
 describe('Snippet Routes', function () {
 
-  beforeEach(done => {
-    new User(exampleUser)
-    .generatePasswordHash(exampleUser.password)
-    .then(user => user.save())
-    .then(user => {
-      this.tempUser = user;
-      return user.generateToken();
-    })
-    .then(token => {
-      this.tempToken = token;
-      done();
-    })
-    .catch(() => done());
-  });
+  describe('POST /api/snippet/:storyId', function() {
 
-  beforeEach(done => {
-    exampleStory.userId = this.tempUser._id.toString();
-    new Story(exampleStory).save()
-    .then(story => {
-      this.tempStory = story;
-      done();
-    })
-    .catch(() => done());
-  });
-
-  afterEach(() => delete exampleStory.userId);
-
-  afterEach(done => {
-    Promise.all([
-      User.remove({}),
-      Story.remove({}),
-    ])
-    .then(() => done())
-    .catch(() => done());
-  });
-
-  describe('POST /api/snippet/:storyId', () => {
-
-    describe('Request with a valid body', () => {
-
-      it.only('should return a snippet and 200 response', done => {
-
-        // console.log('wahts the tempUser', this.tempUser);
-        // console.log('wahts the tempToken', this.tempToken);
-        // console.log('wahts the exampleStory.userId', exampleStory.userId);
-        // console.log('whats the tempStory', this.tempStory);
-        // console.log('wahts the te tempstoryID?', this.tempStory._id);
-
-        request.post(`${url}/api/snippet/${this.tempStory._id}`)
-        .set({Authorization: `Bearer ${this.tempToken}`})
-        .send(exampleSnippet)
-        .end((err, res) => {
-          console.log('bookmark', res.body.bookmark);
-          let date = new Date(res.body.created).toString();
-          expect(res.body.snippetContent).to.equal('And then the storied continued with a user submitted snippet');
-          expect(res.body.pending).to.equal(true);
-          expect(res.body.userId[0]).to.equal(`${this.tempUser._id}`);
-          expect(date).to.not.equal('Invalid Date');
-          expect(res.status).to.equal(200);
-          done();
-        });
-      });
-    });
-
-    describe('Request with an invalid token', function() {
-      it('should respond with a 401 unauthorized error', done => {
-
+    beforeEach(done => {
+      new User(exampleUser)
+      .generatePasswordHash(exampleUser.password)
+      .then(user => user.save())
+      .then(user => {
+        this.tempUser = user;
+        return user.generateToken();
+      })
+      .then(token => {
+        this.tempToken = token;
         done();
-      });
+      })
+      .catch(() => done());
     });
 
-    describe('Request with an invalid story ID', function() {
-      it('should respond with a 404 error not found', done => {
-
+    beforeEach(done => {
+      exampleStory.userId = this.tempUser._id.toString();
+      new Story(exampleStory).save()
+      .then(story => {
+        this.tempStory = story;
         done();
-      });
+      })
+      .catch(() => done());
     });
 
+    afterEach(() => delete exampleStory.userId);
 
-    describe('Request with an invalid body', function() {
-      it('should respond with a 400 invalid body error', done => {
-
-        done();
-      });
+    afterEach(done => {
+      Promise.all([
+        User.remove({}),
+        Story.remove({}),
+      ])
+      .then(() => done())
+      .catch(() => done());
     });
 
+//+##### DO NOT DELETE, please ########
 
-
+    // describe('Request with a valid body', () => {
+    //
+    //   it('should return a snippet and 200 response', done => {
+    //     request.post(`${url}/api/snippet/${this.tempStory._id}`)
+    //     .set({Authorization: `Bearer ${this.tempToken}`})
+    //     .send(exampleSnippet)
+    //     .end((err, res) => {
+    //       let date = new Date(res.body.created).toString();
+    //       expect(res.body.snippetContent).to.equal('And then the story continued with a user submitted snippet');
+    //       expect(res.body.pending).to.equal(true);
+    //       expect(res.body.userId[0]).to.equal(`${this.tempUser._id}`);
+    //       expect(date).to.not.equal('Invalid Date');
+    //       expect(res.status).to.equal(200);
+    //       done();
+    //     });
+    //   });
+    // });
+    //
+    // describe('a request with an invalid token', () => {
+    //   it('should respond with a 401 unauthorized error', done => {
+    //     request.post(`${url}/api/snippet/${this.tempStory._id}`)
+    //     .set({Authorization: 'Bad token'})
+    //     .send(exampleSnippet)
+    //     .end((err, res) => {
+    //       expect(res.status).to.equal(401);
+    //       done();
+    //     });
+    //   });
+    // });
+    //
+    // describe('a request with an invalid story ID', () => {
+    //   it('should respond with a 404 error not found', done => {
+    //     request.post(`${url}/api/snippet/story12345`)
+    //     .set({Authorization: `Bearer ${this.tempToken}`})
+    //     .send(exampleSnippet)
+    //     .end((err, res) => {
+    //       expect(res.status).to.equal(404);
+    //       done();
+    //     });
+    //   });
+    // });
+    //
+    // describe('a bad request with an invalid body', () => {
+    //   it('should respond with a 400 invalid body error', done => {
+    //     request.post(`${url}/api/snippet/${this.tempStory._id}`)
+    //     .set({Authorization: `Bearer ${this.tempToken}`})
+    //     .send('nothing')
+    //     .end((err, res) => {
+    //       expect(res.status).to.equal(400);
+    //       done();
+    //     });
+    //   });
+    // });
 
   });
-
-
-
 });
